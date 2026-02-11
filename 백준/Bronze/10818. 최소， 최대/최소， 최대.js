53e7bf4f8e2e0b35be9e2ad4f1aaf76482a8f250
yang <diddnjswns20@gmail.com>
2026-02-11 23:04:27 +0900

    백준 허브 기본 연동 폴더 제거

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.js
+++
@@ -1,6 +0,0 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
-const input = fs.readFileSync(filePath).toString().trim().split("\n");
-
-const numbers = input[1].split(" ").map(Number);
-console.log(Math.min(...numbers), Math.max(...numbers));
